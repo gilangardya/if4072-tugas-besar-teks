e4c8d241b738f4644d51876bbfbd6f91ec9847a6
X550ZE\David <davidtheoxcalibur@gmail.com>
2018-11-04 09:15:54 +0700

    Finish tokopedia scraping

--- src/data/tokopedia-scraping/tokopediascraping.py
+++ src/data/tokopedia-scraping/tokopediascraping.py
@@ -7,51 +7,65 @@ import os
 import datetime
 from selenium.webdriver.common.by import By
 import sys
+import time
 
-url = "https://www.tokopedia.com/p/handphone-tablet/handphone?page="
+tok_url = "https://www.tokopedia.com/p/handphone-tablet/handphone?ob=5&page="
 
 if(len(sys.argv) != 3):
-	print("Penggunaan: python tokopediascraping.py page-awal page-akhir > file.txt")
-	exit(0)
+    print("Penggunaan: python tokopediascraping.py page-awal page-akhir > file.txt")
+    exit(0)
 
 driver = webdriver.Chrome()
-driver.implicitly_wait(30)
+driver.implicitly_wait(5)
 
 for x in range(int(sys.argv[1]), int(sys.argv[2])):
-    print('--------' + str(x))
-    driver.get(url + str(x))
-    soup_level1=BeautifulSoup(driver.page_source, 'html.parser')
-    for link in soup_level1.find_all("a", {"ng-click":"NgTrack('product', model.department_id)"}):
-        try:
-            url = link["ng-href"]
-            driver.get(url)
-            tab_button = driver.find_element_by_id('review-tab')
-            tab_button.click()
-            is_nextpage = True
-
-            soup_level2=BeautifulSoup(driver.page_source, 'html.parser')
-            title = soup_level2.find("h1", {"class":"rvm-product-title"}).contents
-            price = soup_level2.find("span", {"itemprop":"price"})['content']
-            condition = soup_level2.find("link", {"itemprop":"itemCondition"})['href']
-            while (is_nextpage):
-                soup_level2=BeautifulSoup(driver.page_source, 'html.parser')
-                for review in soup_level2.find_all("div", {"class":"list-box-comment"}):
-                    try:
-                        body = review.find("span", {"class":"review-body"}).contents
-                        reviewer = review.find("a", {"class":"text-black-7 fw-600 text-hover-green"}).contents
-                        date = review.find("span", {"class":"text-black-18 fs-12 ml-8"}).contents
-                        rating = review.find("meta", {"itemprop":"ratingValue"})['content']
-                        a= ["Handphone", title, price, condition, body, rating, reviewer, date, str(datetime.datetime.now()), url]
-                        print (str(a).encode("utf-8"))
-                    except:
-                        pass
+    try:
+        print('--------page' + str(x))
+        driver.get(tok_url + str(x))
+        soup_level1=BeautifulSoup(driver.page_source, 'html.parser')
+        alternate = True
+        for link in soup_level1.find_all("a", {"ng-click":"NgTrack('product', model.department_id)"}):
+            if alternate:
                 try:
-                    next_but = driver.find_element_by_css_selector('.icon-chevron-right-alt.fs-20')
-                    td_p_input = next_but.find_element_by_xpath('..')
-                    td_p_input.click()  
+                    url = link["ng-href"]
+                    driver.get(url)
+                    driver.find_element_by_xpath("//*[@id='review-tab']").click()
+                    soup_level2=BeautifulSoup(driver.page_source, 'html.parser')
+                    title = soup_level2.find("h1", {"class":"rvm-product-title"}).contents[0]
+                    price = soup_level2.find("span", {"itemprop":"price"})['content']
+                    condition = soup_level2.find("link", {"itemprop":"itemCondition"})['href']
+                    for i in range (2,7):
+                        try:
+                            time.sleep(2)
+                            driver.find_element_by_xpath("//*[@class='product-review-filter']/ul/li["+str(i)+"]").click()
+                            is_nextpage = True
+                            while (is_nextpage):
+                                time.sleep(2)
+                                for review in driver.find_elements_by_xpath("//*[@class='list-box-comment']"):
+                                    try:
+                                        review_source = BeautifulSoup(review.get_attribute("innerHTML"), 'html.parser')
+                                        body = review_source.find("span", {"class":"review-body"}).contents[0]
+                                        reviewer = review_source.find("a", {"class":"text-black-7 fw-600 text-hover-green"})
+                                        if reviewer is None:
+                                            reviewer = review_source.find("span", {"class":"text-black-7 fw-600"})
+                                        reviewer = reviewer.contents[0]
+                                        date = review_source.find("span", {"class":"text-black-18 fs-12 ml-8"}).contents[0]
+                                        rating = review_source.find("meta", {"itemprop":"ratingValue"})['content']
+                                        a= ["Handphone", title, price, condition, body, rating, reviewer, date, str(datetime.datetime.now()), url]
+                                        print (str(a[0])+"||||"+str(a[1])+"||||"+str(a[2])+"||||"+str(a[3])+"||||"+str(a[4])+"||||"+str(a[5])+"||||"+str(a[6])+"||||"+str(a[7])+"||||"+str(a[8])+"||||"+str(a[9]))
+                                    except:
+                                        pass
+
+                                try:
+                                    driver.find_element_by_xpath("//div[@class='pagination pull-right']//i[@class='icon-chevron-right-alt fs-20']").click()
+                                except:
+                                    is_nextpage = False
+                        except:
+                            pass
                 except:
-                    is_nextpage = False
-            
-        except: 
-            pass
+                    pass
+            alternate = not(alternate)
+    except:
+        pass
+
 driver.quit()
